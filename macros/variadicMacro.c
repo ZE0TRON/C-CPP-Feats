725c610a5b1c5ca4cdc021a4239e806b84e8ba21
ZE0TRON <cmpbilge@gmail.com>
2019-09-24 11:54:23 +0300

    variadic macros updated

--- macros/variadicMacro.c
+++ macros/variadicMacro.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #define eprintf(args...) fprintf (stderr, args)
-
+#define jprintf(format,...) fprintf (stderr,format, __VA_ARGS__)
 int main(){
 
 FILE *fptr;
 fptr = fopen("variadic.txt","w");
-eprintf ("%s:%d: ", fptr, 10);
+eprintf ("%s:%d:\n", fptr, 10);
 eprintf("Success!\n");
+jprintf ("%s:%d: ", fptr, 10);
 fclose(fptr);
 }
